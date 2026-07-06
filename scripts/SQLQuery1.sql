cbe06e69ca8f6098007964231ac73bc6441a0672
Danishdbu <94746768+Danishdbu@users.noreply.github.com>
2026-07-06 22:50:27 +0530

    Add files via upload

---
+++ scripts/SQLQuery1.sql
@@ -0,0 +1,13 @@
+-- create database
+use master;
+ CREATE DATABASE Datawarehouse;
+ USE DataWarehouse ;
+
+ -- CREATE SCHEMA
+
+CREATE SCHEMA bronze;
+GO
+CREATE SCHEMA silver;
+GO
+CREATE SCHEMA gold;
+GO
